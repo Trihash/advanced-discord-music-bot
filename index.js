abfd1f18ee827c5e1bd1e04c76a325b20bc14cab
emirats <59978524+emirats@users.noreply.github.com>
2020-02-02 17:59:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,72 @@ client.once('disconnect', () => {
 	console.log('Disconnect!');
 });
 
+const Discord = require('discord.js')
+const colors = require("colors");
+
+const {
+	prefix,
+	token,
+} = require('./config.json');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
+});
+
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+
+
+client.on('ready',() => {
+	let memberCount = client.users.size;
+	let serverCount = client.guilds.size;
+	con(
+	  `${"-".repeat(65)}\n`                         + 
+	  "> Bienvenue sur les Logs.   \n" + 
+	  `${"-".repeat(65)}\n`                         +  
+	  "> Information du client : \n"                     +
+	  `> Nom du client    : ${client.user.tag}!\n`          + 
+	  `> ID du client     : ${client.user.id}\n`            +
+	  `${"-".repeat(65)}\n`                         +
+	  "> Stats client : \n"                              +
+	  `> Utilisateurs : ${memberCount}\n`             +
+	  `> Serveurs     : ${serverCount}\n`             +
+	  `${"-".repeat(65)}\n`
+	);
+  });
+  
+  colors.setTheme({
+	silly: 'rainbow',
+	input: 'grey',
+	verbose: 'cyan',
+	prompt: 'grey',
+	info: 'green',
+	data: 'grey',
+	help: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red',
+  });
+  
+  client.on('ready', () => {
+	console.log(colors.cyan('                         _         __          __ '))
+	console.log(colors.cyan('   ____ ___  __  _______(_)____   / /_  ____  / /_'))
+	console.log(colors.cyan('  / __ `__ \/ / / / ___/ / ___/  / __ \/ __ \/ __/'))
+	console.log(colors.cyan(' / / / / / / /_/ (__  ) / /__   / /_/ / /_/ / /_  '))
+	console.log(colors.cyan('/_/ /_/ /_/\__,_/____/_/\___/  /_.___/\____/\__/  '))
+
+  });
+
 client.on('message', async message => {
 	if (message.author.bot) return;
 	if (message.channel.type !== 'text') {
