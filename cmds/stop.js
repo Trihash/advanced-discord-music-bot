28c196a3cd4e7e0b14a8f37caa3cbc1dbb99db16
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-02-02 12:37:43 +0100

    Add try catch

--- cmds/stop.js
+++ cmds/stop.js
@@ -2,11 +2,16 @@ const Discord = require('discord.js')
 const ytdl = require('ytdl-core')
 
 function stop(message, serverQueue) {
-    if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
-    if (!serverQueue) return message.channel.send('There is no queue!');
-	serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-    message.react('👋')
+    try {
+        if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+        if (!serverQueue) return message.channel.send('There is no queue!');
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+        message.react('👋')
+    } catch (err) {
+        console.log(err)
+        message.react('🖐')
+    }
 }
 
 module.exports = stop
